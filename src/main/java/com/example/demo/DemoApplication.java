69eecb7284a3367e3336d78bfcfc7dacff683a88
yugojim <yugojim@gmail.com>
2024-08-23 16:30:55 +0800

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class DemoApplication {
+		
 	@RequestMapping("/")
 	public String home() {
 		System.out.println("hi");
@@ -25,3 +28,4 @@ public class DemoApplication {
 	}
 
 }
+
