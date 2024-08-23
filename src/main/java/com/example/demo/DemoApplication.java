c218d10d075a3d22d9b5c92ac3b0ed9f760740f3
yugojim <yugojim@gmail.com>
2024-08-23 14:38:46 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class DemoApplication {
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("hi");
+		return "Hello World";
+	}
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
